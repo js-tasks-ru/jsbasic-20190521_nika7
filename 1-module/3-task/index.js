8df72f89dcdaf116e20ebe9f21919df895408914
Nika <nikasampetova@gmail.com>
2019-06-03 02:44:08 +0300

    done task 3 module 2

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -5,7 +5,19 @@
  * @param   {string} str -  входные данные
  * @returns {{min:number, max:number}}  объект
  */
+let inputData = '1, -5.8 или 10, хотя 34 + -5.3 и 73';
 function getMinMax(str) {
-
+let minmaxObj = {min: null, max: null}
+let numbers = str.match(/-?(\d+(\.\d+)?)/ig)
+numbers = numbers.map(function(name) {
+    return Number(name);
+});
+numbers.sort(function(a, b) {
+    return a - b;
+});
+minmaxObj['min'] = numbers[0]
+minmaxObj['max'] = numbers[numbers.length - 1]
+return minmaxObj
 }
+getMinMax(inputData)
 
