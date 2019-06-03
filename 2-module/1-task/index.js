02d340a9153609f1a25f1f23429a5ab3dcca55cc
Nika <nikasampetova@gmail.com>
2019-06-04 00:20:01 +0300

    done task 1-2 module 2

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -3,5 +3,16 @@
  * @param {Object} obj - клонируем объект
  * @returns {Object}
  */
+let obj = {test: { test2: { test3: '1' } }};
 function clone (obj) {
+    if (obj == null || typeof obj !== 'object') return obj;
+    let copy = {}
+    for (let o in obj){
+        copy[o] = clone(obj[o])
+    }
+    return copy;
 }
+
+console.log(clone(obj).test.test2 !== obj.test.test2)
+console.log(clone(obj).test.test2.test3 === obj.test.test2.test3)
+
