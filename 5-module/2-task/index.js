f7d8fe26dda535d1b86438ca9416c5115554bf06
Nika <nikasampetova@gmail.com>
2019-07-04 00:10:35 +0300

    done 1 error test task 2 module 5

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -16,17 +16,101 @@
  * @constructor
  */
 function SortableTable(items) {
+    let headRow = document.createElement('thead')
+    headRow.innerHTML = '<tr>\n' + '<td>Name</td>\n' + '<td>Age</td>\n' + '<td>Salary</td>\n' + '<td>City</td>\n' + '</tr>'
+    let bodyRow = document.createElement('tbody')
+    items.forEach(function (element) {
+        let innerBody = document.createElement('tr')
+        innerBody.innerHTML += '<td>' + element.name + '</td>\n'
+        innerBody.innerHTML += '<td>' + element.age + '</td>\n'
+        innerBody.innerHTML += '<td>' + element.salary + '</td>\n'
+        innerBody.innerHTML += '<td>' + element.city + '</td>\n'
+        bodyRow.appendChild(innerBody)
+    })
+
 
     /**
      * @property {Element} - обязательно свойство, которое ссылается на элемент <table>
      */
     this.el = document.createElement('table');
+    this.el.appendChild(headRow)
+    this.el.appendChild(bodyRow)
 
     /**
      * Метод выполняет сортировку таблицы
      * @param {number} column - номер колонки, по которой нужно выполнить сортировку (отсчет начинается от 0)
      * @param {boolean} desc - признак того, что сортировка должна идти в обратном порядке
      */
-    this.sort = function (column, desc = false) { };
+    this.sort = function (column, desc = false)
+    {
+        let bodySort = this.el.lastElementChild
+        let allBodyRows = bodySort.querySelectorAll('tr')
+        let allBodyRows1 = Array.prototype.slice.call(allBodyRows)
+        let newTbody = document.createElement('tbody')
+        if (!desc) {
+            allBodyRows1.sort(function (a, b) {
+                if (!isNaN(Number(a.children[column].innerHTML))) {
+                    if (Number(a.children[column].innerHTML)< Number(b.children[column].innerHTML)) {
+                        return -1;
+                    }
+                } else {
+                    if (a.children[column].innerHTML < b.children[column].innerHTML) {
+                        return -1;
+                    }
+                }
+            })
+            allBodyRows1.forEach(function (el) {
+                newTbody.appendChild(el)
+
+            })
+        }  else {
+            allBodyRows1.sort(function (a, b) {
+                if (!isNaN(Number(a.children[column].innerHTML))) {
+                    if (Number(a.children[column].innerHTML) > Number(b.children[column].innerHTML)) {
+                        return 1;
+                    }
+                } else {
+                    if (a.children[column].innerHTML > b.children[column].innerHTML) {
+                        return 1;
+                    }
+                }
+            })
+            allBodyRows1.forEach(function (el) {
+                newTbody.appendChild(el)
+
+            })
+        }
+        this.el.appendChild(newTbody)
+
+    };
 }
+
+
+// function Button({el, options}) {
+//     // this = {}
+//
+//     this.el = el;
+//     this.text = options.text;
+//
+//
+//     this.click = function () {
+//
+//     }
+// }
+//
+// // app.js
+//
+// const submit = new Button({
+//     el: document.body,
+//     options: {
+//         text: 'Отправить'
+//     }
+// });
+//
+// const scrollDow = new Button({
+//     el: document.body,
+//     options: {
+//         text: 'Вниз'
+//     }
+// })
 
