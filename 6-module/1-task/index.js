e48360a6965130dda38e8a13482aaa9341eee8c8
Nika <nikasampetova@gmail.com>
2019-08-26 00:10:53 +0300

    done test task 2 module 5

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -31,13 +31,21 @@
                 innerBody.innerHTML += '<td>' + element.age + '</td>\n'
                 innerBody.innerHTML += '<td>' + element.salary + '</td>\n'
                 innerBody.innerHTML += '<td>' + element.city + '</td>\n'
-                innerBody.innerHTML += '<td><a href="#delete" onclick="table.onRemoved(' + element.id + ')">X</a></td>\n'
+                innerBody.innerHTML += '<td><a href="#delete">X</a></td>\n'
                 bodyRow.appendChild(innerBody)
             })
 
             this.el.appendChild(headRow)
             this.el.appendChild(bodyRow)
             this.el.classList.add('pure-table')
+            let self = this;
+            this.el.addEventListener("click", function(event) {
+               if (!event.target.hasAttribute('href')) return
+                let id = event.target.parentElement.parentElement.id
+                self.onRemoved(Number(id))
+               // console.log(event.target)
+            });
+
         }
 
         /**
@@ -45,8 +53,18 @@
          * @param {number} id - идентификатор удаляемого пользователя
          */
         onRemoved(id) {
-            let elToRemove = document.getElementById(id)
-            elToRemove.remove()
+            // let elToRemove = document.getElementById(id)
+            // elToRemove.remove()
+            // elToRemove.innerHTML = "";
+            let row_num = 1;
+            for (let i in this.data){
+                if(this.data[i].id === id) {
+                    delete this.data[i];
+                    break
+                }
+                row_num += 1;
+            }
+            this.el.deleteRow(row_num)
             console.log(`Из таблицы удален пользователь ${id}`)
 
 
